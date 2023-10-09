5dc935f69d7ba10950614a910c497bd540d36e77
divij1999 <divijsumbly@gmail.com>
2023-10-09 15:38:19 +0530

    First commit

---
+++ server.js
@@ -0,0 +1,10 @@
+const http = require("http");
+
+const HOST = "127.0.0.1";
+const PORT = 3000;
+
+const server = http.createServer((req, res) => {
+  res.end("hello world");
+});
+
+server.listen(PORT, HOST);
